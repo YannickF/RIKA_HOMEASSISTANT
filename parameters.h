0211c28eeaea1cad48c7026f2065dcd10e868509
Yannick F <y.fetre@laposte.net>
2022-11-26 18:44:28 +0100

    Create parameters.h

---
+++ parameters.h
@@ -0,0 +1,4 @@
+const char* ha_ip = "192.168.5.25";   // IP serveur Home Assistant
+int ha_port = 8123;                   // port HomeAssistant
+const char* webhook= "-0sqdfshvnjSERSRT6EGWUG";  //webhook for homeassistant
+const char* IP_NTP_SERVER = "192.168.5.10";     // NTP server IP address
